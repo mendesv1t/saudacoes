6e9b44e29849cf8601cbcc2ea1bc51eef6b4d5ad
Vitória Chaves <vitoriagrier@gmail.com>
2024-05-19 11:40:22 -0300

    corrige lógica de verificação dos ids e contador

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int contador = 0;
 
 void * saudacao(void * arg) {
 
-    int id = * (int *) arg;
+    int id = *(int *) arg;
 
     /*
      * As threads atualizam a variável contador assim que disparam suas saudações, para isso, realizamos
@@ -31,73 +31,83 @@ void * saudacao(void * arg) {
          *  executará após a thread 2 sinalizar o desbloqueio.
          * */
 
-        if (id == 0 && contador == 0) {
+        if (id == 0) {
             pthread_mutex_lock(&mutex);
-            printf("Thread (%d) :", id+1);
-            printf(SAUDACAO_MARIA);
-            printf("\n");
-            contador++;
-            pthread_mutex_unlock(&mutex);
+            if (contador == 0) {
+                printf("Thread (%d) :", id + 1);
+                printf(SAUDACAO_MARIA);
+                printf("\n");
+                contador++;
             }
+            pthread_mutex_unlock(&mutex);
         }
-        if (id == 1 && contador == 1) {
-                pthread_mutex_lock(&mutex);
-                printf("Thread (%d) :", id+1);
+
+        if (id == 1) {
+            pthread_mutex_lock(&mutex);
+            if (contador == 1) {
+                printf("Thread (%d) :", id + 1);
                 printf(SAUDACAO_JOSE);
                 printf("\n");
                 contador++;
-                pthread_cond_signal(&cond_sente_se);
-                pthread_mutex_unlock(&mutex);
             }
+            pthread_cond_signal(&cond_sente_se);
+            pthread_mutex_unlock(&mutex);
         }
 
-        /* A thread 3 aguarda um signal da variável condicional cond_sente_se, e ela só
-        *  executará após a thread 2 sinalizar o desbloqueio.
-        * */
+            /* A thread 3 aguarda um signal da variável condicional cond_sente_se, e ela só
+            *  executará após a thread 2 sinalizar o desbloqueio.
+            * */
 
         else if (id == 2) {
+
             if (contador < 2) {
                 pthread_cond_wait(&cond_sente_se, &mutex);
             }
-            else if (contador == 2) {
-                pthread_mutex_lock(&mutex);
+
+            pthread_mutex_lock(&mutex);
+            if (contador == 2) {
                 contador++;
-                printf("Thread (%d) :", id+1);
+                printf("Thread (%d) :", id + 1);
                 printf(SENTE_SE);
                 printf("\n");
-                pthread_cond_broadcast(&cond_sente_se);
-                pthread_mutex_unlock(&mutex);
             }
+            pthread_cond_broadcast(&cond_sente_se);
+            pthread_mutex_unlock(&mutex);
         }
 
-        /* As threads 4 e 5 só podem ser executadas após a execução da thread 3:
-        *  Logo a alternativa foi, a thread 3 sinaliza pela variável condicional cond_sente_se,
-        *  disparando um broadcast, que a partir daí, tanto a thread 4 ou 5 poderão ser executadas.
-        * */
+            /* As threads 4 e 5 só podem ser executadas após a execução da thread 3:
+            *  Logo a alternativa foi, a thread 3 sinaliza pela variável condicional cond_sente_se,
+            *  disparando um broadcast, que a partir daí, tanto a thread 4 ou 5 poderão ser executadas.
+            * */
 
         else if (id == 3) {
+
             if (contador < 3) {
                 pthread_cond_wait(&cond_sente_se, &mutex);
-            } else if (contador == 3) {
-                pthread_mutex_lock(&mutex);
-                printf("Thread (%d) :", id+1);
+            }
+
+            pthread_mutex_lock(&mutex);
+            if (contador == 3) {
+                printf("Thread (%d) :", id + 1);
                 printf(TCHAU_MARIA);
                 printf("\n");
                 contador++;
-                pthread_mutex_unlock(&mutex);
             }
-        }
-        else if (id == 4) {
+            pthread_mutex_unlock(&mutex);
+        } else if (id == 4) {
+
             if (contador < 4) {
                 pthread_cond_wait(&cond_sente_se, &mutex);
-            } else if (contador == 4) {
-                pthread_mutex_lock(&mutex);
-                printf("Thread (%d) :", id+1);
+            }
+
+            pthread_mutex_lock(&mutex);
+            if (contador == 4) {
+                printf("Thread (%d) :", id + 1);
                 printf(TCHAU_JOSE);
                 printf("\n");
                 contador++;
-                pthread_mutex_unlock(&mutex);
             }
+            pthread_mutex_unlock(&mutex);
         }
     }
     pthread_exit(NULL);
