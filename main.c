edc610d41bc00879e276a96e193cf69b279a3d16
Vitória Chaves <vitoriagrier@gmail.com>
2024-05-19 11:29:29 -0300

    adiciona explicações no código

--- main.c
+++ main.c
@@ -18,7 +18,19 @@ void * saudacao(void * arg) {
 
     int id = * (int *) arg;
 
+    /*
+     * As threads atualizam a variável contador assim que disparam suas saudações, para isso, realizamos
+     * lock e unlock da variável mutex, para garantir atomicidade nos acessos a essa variável, que possui
+     * escopo global.
+     * */
+
     while (contador < 5) {
+
+        /* As threads 1 e 2 não possuem condição de execução, a não ser que sejam executadas antes da thread 3:
+         *  Logo a alternativa foi, a thread 3 aguarda um signal da variável condicional cond_sente_se, e ela só
+         *  executará após a thread 2 sinalizar o desbloqueio.
+         * */
+
         if (id == 0 && contador == 0) {
             pthread_mutex_lock(&mutex);
             printf("Thread (%d) :", id+1);
@@ -38,6 +50,11 @@ void * saudacao(void * arg) {
                 pthread_mutex_unlock(&mutex);
             }
         }
+
+        /* A thread 3 aguarda um signal da variável condicional cond_sente_se, e ela só
+        *  executará após a thread 2 sinalizar o desbloqueio.
+        * */
+
         else if (id == 2) {
             if (contador < 2) {
                 pthread_cond_wait(&cond_sente_se, &mutex);
@@ -52,6 +69,12 @@ void * saudacao(void * arg) {
                 pthread_mutex_unlock(&mutex);
             }
         }
+
+        /* As threads 4 e 5 só podem ser executadas após a execução da thread 3:
+        *  Logo a alternativa foi, a thread 3 sinaliza pela variável condicional cond_sente_se,
+        *  disparando um broadcast, que a partir daí, tanto a thread 4 ou 5 poderão ser executadas.
+        * */
+
         else if (id == 3) {
             if (contador < 3) {
                 pthread_cond_wait(&cond_sente_se, &mutex);
@@ -93,7 +116,7 @@ void criarThreads(int M) {
         }
     }
 
-    for (t=0; t<M; t++) {
+    for (t = 0; t < M; t++) {
         if (pthread_join(tid[t], NULL)) {
             printf("--ERRO: pthread_join() \n"); exit(-1);
         }
